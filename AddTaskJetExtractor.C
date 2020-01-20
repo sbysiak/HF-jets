aad3c89a35c14d05d66735d8ba6b43c09ee44941
Sebastian Bysiak <sebastian.bysiak@gmail.com>
2020-01-20 16:13:37 +0100

    Old way (Nov. 2019) with data-MC differences

---
+++ AddTaskJetExtractor.C
@@ -0,0 +1,111 @@
+AliAnalysisTaskJetExtractor* AddTaskJetExtractor (TString trackArray="tracks", TString clusterArray="", TString jetArray="Jet_AKTChargedR040_tracks_pT0150_E_scheme", TString rhoObject="", Double_t jetRadius=0.4, const char* taskNameSuffix="allJets")
+{
+
+    cout << "\n\n\n\n\n\n\n MOJE ADD TASK !!!!! \n\n\n\n\n\n\n";
+
+
+
+
+
+
+    TString mode = TString(taskNameSuffix);
+    if (mode.EqualTo("allJets")){    /***** DATA *****/
+        // task JetExt depends on
+
+
+
+        /////// Jet Extractor Task ///
+        AliRDHFJetsCutsVertex* cuts = new AliRDHFJetsCutsVertex("jetCuts");
+        AliAnalysisTaskJetExtractor* myTask = AliAnalysisTaskJetExtractor::AddTaskJetExtractor(trackArray, clusterArray, jetArray, rhoObject, jetRadius, cuts, taskNameSuffix);
+
+        myTask->SelectCollisionCandidates(AliVEvent::kINT7);
+        myTask->SetForceBeamType(AliAnalysisTaskEmcal::kpp);
+        myTask->SetVzRange(-10,10);
+        myTask->SetNeedEmcalGeom(kFALSE);
+        myTask->SetSaveConstituents(1);
+        myTask->SetSaveConstituentsIP(1);
+        myTask->SetSaveConstituentPID(1);
+        myTask->SetSaveJetShapes(1);
+        myTask->SetSaveJetSplittings(1);
+        myTask->SetSaveSecondaryVertices(1);
+        myTask->SetSaveTriggerTracks(1);
+        myTask->SetSaveMCInformation(1);
+        myTask->GetJetTree()->AddExtractionPercentage(0,5   , 0.1);
+        myTask->GetJetTree()->AddExtractionPercentage(5,10  , 0.3);
+        myTask->GetJetTree()->AddExtractionPercentage(10,20 , 0.6);
+        myTask->GetJetTree()->AddExtractionPercentage(20,40 , 1.0);
+        myTask->GetJetTree()->AddExtractionPercentage(40,200, 1.0);
+
+        myTask->GetJetContainer(0)->SetJetEtaLimits(-(0.9-jetRadius), (0.9-jetRadius));
+
+        return myTask;
+    }
+    else{                            /***** MC *****/
+        // task JetExt depends on
+
+
+        /////// Jet Extractor Task ///
+        AliRDHFJetsCutsVertex* cuts = new AliRDHFJetsCutsVertex("jetCuts");
+        AliAnalysisTaskJetExtractor* myTask = AliAnalysisTaskJetExtractor::AddTaskJetExtractor(trackArray, clusterArray, jetArray, rhoObject, jetRadius, cuts, taskNameSuffix);
+
+        myTask->SetIsPythia(kTRUE);
+        myTask->SelectCollisionCandidates(AliVEvent::kINT7);
+        myTask->SetForceBeamType(AliAnalysisTaskEmcal::kpp);
+        myTask->SetVzRange(-10,10);
+        myTask->SetNeedEmcalGeom(kFALSE);
+        myTask->SetSaveConstituents(1);
+        myTask->SetSaveConstituentsIP(1);
+        myTask->SetSaveConstituentPID(1);
+        myTask->SetSaveJetShapes(1);
+        myTask->SetSaveJetSplittings(1);
+        myTask->SetSaveSecondaryVertices(1);
+        myTask->SetSaveTriggerTracks(1);
+        myTask->SetSaveMCInformation(1);
+
+        myTask->SetUseBuiltinEventSelection(kFALSE);
+
+        myTask->GetJetContainer(0)->SetJetRadius(jetRadius);
+        myTask->GetJetContainer(0)->SetPercAreaCut(0.557);
+        myTask->GetJetContainer(0)->SetJetEtaLimits(-(0.9-jetRadius), (0.9-jetRadius));
+
+        myTask->GetTrackContainer(0)->SetTrackFilterType(AliEmcalTrackSelection::kCustomTrackFilter);
+        myTask->GetTrackContainer(0)->SetAODFilterBits((1<<4)|(1<<9));
+
+        myTask->SetNumberOfPtHardBins(21);
+        Int_t arr[22] = {0,5,7,9,12,16,21,28,36,45,57,70,85,99,115,132,150,169,190,212,235,1000000};
+        TArrayI bins(22, arr);
+        myTask->SetUserPtHardBinning(bins);
+
+        if (mode.EqualTo("bJets")){
+            myTask->GetJetTree()->AddExtractionPercentage(0,5   , 0.1);
+            myTask->GetJetTree()->AddExtractionPercentage(5,10  , 0.3);
+            myTask->GetJetTree()->AddExtractionPercentage(10,20 , 0.6);
+            myTask->GetJetTree()->AddExtractionPercentage(20,40 , 1.0);
+            myTask->GetJetTree()->AddExtractionPercentage(40,200, 1.0);
+            myTask->GetJetTree()->AddExtractionJetTypeHM(5);
+            return myTask;
+        }
+        if (mode.EqualTo("cJets")){ // 2x less than b except for last
+            myTask->GetJetTree()->AddExtractionPercentage(0,5   , 0.05);
+            myTask->GetJetTree()->AddExtractionPercentage(5,10  , 0.15);
+            myTask->GetJetTree()->AddExtractionPercentage(10,20 , 0.3);
+            myTask->GetJetTree()->AddExtractionPercentage(20,40 , 0.5);
+            myTask->GetJetTree()->AddExtractionPercentage(40,200, 1.0);
+            myTask->GetJetTree()->AddExtractionJetTypeHM(4);
+            return myTask;
+        }
+        if (mode.EqualTo("udsgJets")){ // 10x less then b except for last
+            myTask->GetJetTree()->AddExtractionPercentage(0,5   , 0.0005);
+            myTask->GetJetTree()->AddExtractionPercentage(5,10  , 0.03);
+            myTask->GetJetTree()->AddExtractionPercentage(10,20 , 0.1); // +
+            myTask->GetJetTree()->AddExtractionPercentage(20,40 , 0.2);
+            myTask->GetJetTree()->AddExtractionPercentage(40,80 , 0.6);
+            myTask->GetJetTree()->AddExtractionPercentage(80,200, 1.0);
+            myTask->GetJetTree()->AddExtractionJetTypeHM(1);
+            myTask->GetJetTree()->AddExtractionJetTypeHM(3);
+            return myTask;
+        }
+
+
+    }
+}
---
+++ runAnalysis.C
@@ -0,0 +1,219 @@
+// include the header of your analysis task here! for classes already compiled by aliBuild,
+// precompiled header files (with extension pcm) are available, so that you do not need to
+// specify includes for those. for your own task however, you (probably) have not generated a
+// pcm file, so we need to include it explicitly
+// #include "AliAnalysisTaskMyTask.h"
+
+void runAnalysis()
+{
+    // set if you want to run the analysis locally (kTRUE), or on grid (kFALSE)
+    Bool_t local = kFALSE;
+    // if you run on grid, specify test mode (kTRUE) or full grid model (kFALSE)
+    Bool_t gridTest = kFALSE;
+
+    // since we will compile a class, tell root where to look for headers
+#if !defined (__CINT__) || defined (__CLING__)
+    // ROOT6
+    gInterpreter->ProcessLine(".include $ROOTSYS/include");
+    gInterpreter->ProcessLine(".include $ALICE_ROOT/include");
+#else
+    // ROOT5
+    gROOT->ProcessLine(".include $ROOTSYS/include");
+    gROOT->ProcessLine(".include $ALICE_ROOT/include");
+#endif
+
+    // create the analysis manager
+    AliAnalysisManager *mgr = new AliAnalysisManager("AnalysisTaskExample");
+    AliAODInputHandler *aodH = new AliAODInputHandler();
+    mgr->SetInputEventHandler(aodH);
+
+
+    // compile the class and load the add task macro
+    // here we have to differentiate between using the just-in-time compiler
+    // from root6, or the interpreter of root5
+#if !defined (__CINT__) || defined (__CLING__)
+    gInterpreter->LoadMacro("AliAnalysisTaskJetExtractor.cxx++g");
+    // AliAnalysisTaskJetExtractor *task = reinterpret_cast<AliAnalysisTaskJetExtractor*>(gInterpreter->ExecuteMacro("AddTaskJetExtractor.C"));
+
+    //                            /***** DATA *****/
+/*    TMacro physicsSelection_Add(gSystem->ExpandPathName("$ALICE_PHYSICS/OADB/macros/AddTaskPhysicsSelection.C"));
+    AliPhysicsSelectionTask* physicsSelectionTask = reinterpret_cast<AliPhysicsSelectionTask*>(physicsSelection_Add.Exec("kFALSE, kTRUE"));
+
+    TMacro JetExtractor_R04_JF_Add(gSystem->ExpandPathName("$ALICE_PHYSICS/PWGJE/EMCALJetTasks/macros/AddTaskEmcalJet.C"));
+    AliEmcalJetTask* JetExtractor_R04_JF_Task = reinterpret_cast<AliEmcalJetTask*>(JetExtractor_R04_JF_Add.Exec("\"\", \"\", AliJetContainer::antikt_algorithm, 0.4, AliJetContainer::kChargedJet, 0.15, 0.3, 0.005, AliJetContainer::E_scheme, \"Jet\", 0., kTRUE, kFALSE"));
+
+    JetExtractor_R04_JF_Task->SetNeedEmcalGeom(kFALSE);
+    JetExtractor_R04_JF_Task->SelectCollisionCandidates(AliVEvent::kINT7);
+    AliTrackContainer* trackCont = new AliTrackContainer("tracks");
+    trackCont->SetFilterHybridTracks(kTRUE);
+    trackCont->SetParticlePtCut(0.15);
+    JetExtractor_R04_JF_Task->AdoptParticleContainer(trackCont);
+
+    AliAnalysisTaskJetExtractor *task = reinterpret_cast<AliAnalysisTaskJetExtractor*>(gInterpreter->ExecuteMacro("AddTaskJetExtractor.C(\"tracks\", \"\", \"Jet_AKTChargedR040_tracks_pT0150_E_scheme\", \"\", 0.4, \"allJets\")"));
+    AliAnalysisTaskJetExtractor *task2 = reinterpret_cast<AliAnalysisTaskJetExtractor*>(gInterpreter->ExecuteMacro("AddTaskJetExtractor.C(\"tracks\", \"\", \"Jet_AKTChargedR040_tracks_pT0150_E_scheme\", \"\", 0.4, \"allJets\")"));
+*/
+
+
+//                            /***** MC *****/
+    TMacro JetExtractor_JF_PYTHIA_DetLevel_Add(gSystem->ExpandPathName("$ALICE_PHYSICS/PWGJE/EMCALJetTasks/macros/AddTaskEmcalJet.C"));
+    AliEmcalJetTask* JetExtractor_JF_PYTHIA_DetLevel_Task = reinterpret_cast<AliEmcalJetTask*>(JetExtractor_JF_PYTHIA_DetLevel_Add.Exec("\"\", \"\", AliJetContainer::antikt_algorithm, 0.4, AliJetContainer::kChargedJet, 0.15, 0.3, 0.005, AliJetContainer::E_scheme, \"JetPY\", 0., kTRUE, kFALSE"));
+
+    JetExtractor_JF_PYTHIA_DetLevel_Task->SetNeedEmcalGeom(kFALSE);
+    // JetExtractor_JF_PYTHIA_DetLevel_Task->SelectCollisionCandidates(AliVEvent::kINT7);
+    AliTrackContainer* trackCont = new AliTrackContainer("tracks");
+    trackCont->SetFilterHybridTracks(kTRUE);
+    trackCont->SetParticlePtCut(0.15);
+    JetExtractor_JF_PYTHIA_DetLevel_Task->AdoptParticleContainer(trackCont);
+    JetExtractor_JF_PYTHIA_DetLevel_Task->GetTrackContainer(0)->SetTrackFilterType(AliEmcalTrackSelection::kCustomTrackFilter);
+    JetExtractor_JF_PYTHIA_DetLevel_Task->GetTrackContainer(0)->SetAODFilterBits((1<<4)|(1<<9));
+
+    AliAnalysisTaskJetExtractor *task_b     = reinterpret_cast<AliAnalysisTaskJetExtractor*>(gInterpreter->ExecuteMacro("AddTaskJetExtractor.C(\"tracks\", \"\", \"JetPY_AKTChargedR040_tracks_pT0150_E_scheme\", \"\", 0.4, \"bJets\")"));
+    AliAnalysisTaskJetExtractor *task_c     = reinterpret_cast<AliAnalysisTaskJetExtractor*>(gInterpreter->ExecuteMacro("AddTaskJetExtractor.C(\"tracks\", \"\", \"JetPY_AKTChargedR040_tracks_pT0150_E_scheme\", \"\", 0.4, \"cJets\")"));
+    AliAnalysisTaskJetExtractor *task_light = reinterpret_cast<AliAnalysisTaskJetExtractor*>(gInterpreter->ExecuteMacro("AddTaskJetExtractor.C(\"tracks\", \"\", \"JetPY_AKTChargedR040_tracks_pT0150_E_scheme\", \"\", 0.4, \"udsgJets\")"));
+
+
+
+#else
+    // gROOT->LoadMacro("AliAnalysisTaskJetExtractor.cxx++g");
+    // gROOT->LoadMacro("AddTaskJetExtractor.C");
+    // AliAnalysisTaskJetExtractor *task = AddTaskJetExtractor();
+#endif
+
+
+    if(!mgr->InitAnalysis()) return;
+    mgr->SetDebugLevel(0);
+    mgr->PrintStatus();
+    mgr->SetUseProgressBar(1, 25);
+
+/************************
+    globalvariables.C
+
+    TString kJetAkTName(Form("Jet_AKTChargedR040_PicoTracks_pT0150_pt_scheme"));
+    TString kJetRhokTName(Form("Jet_KTChargedR040_PicoTracks_pT0150_pt_scheme"));
+    Double_t kAreaCut = 0.;
+    Double_t kJetLeadingTrackBias = 0;
+    Double_t kClusPtCut = 0.30;
+    Double_t kTrackPtCut = 0.15;
+    Double_t kPartLevPtCut = 0;
+    Double_t kGhostArea = 0.01;
+    Double_t kMaxTrackPt = 1000;
+    Double_t kJetPtCut = 1;
+    //boost::v1_53_0 cgal::v4.4 fastjet::v3.0.6_1.012
+    ////// OTHER EMCAL RELATED VARIABLES ////
+    const char* kDataPeriod="LHC12c";
+    TString kMatchingChainStr="16:512:includeNoITS=kFALSE doProp=kFALSE doAttemptProp=kTRUE isMC=kFALSE";
+    TString kClusterName="EmcCaloClusters";
+    TString kRhoTaskName="ExternalRhoTask";
+    TString kTracksName="PicoTracks";
+*************************/
+
+    if(local) {
+        // if you want to run locally, we need to define some input
+        TChain* chain = new TChain("aodTree");
+        // add a few files to the chain (change this so that your local files are added)
+        // chain->Add("AliAOD.root");
+        // chain->Add("data/__alice__sim__2016__LHC16h3__10__244480__AOD__088__AliAOD.root");
+        chain->Add("data_660/__alice__sim__2017__LHC17f8f__20__257630__001__AliAOD.root");
+        // chain->Add("data/__alice__data__2017__LHC17p__10__000282341__pass1_FAST__AOD208__0001__AliAOD.root");
+        // chain->Add("data/__alice__data__2017__LHC17p__000282341__pass1_FAST__AOD208__AliAOD_2/0002/AliAOD.root");
+        // chain->Add("data/__alice__data__2017__LHC17q__000282366__pass1_FAST__AOD208__AliAOD_2/0001/AliAOD.root");
+        // chain->Add("data/__alice__data__2017__LHC17q__000282366__pass1_FAST__AOD208__AliAOD_2/0002/AliAOD.root");
+
+        // start the analysis locally, reading the events from the tchain
+        mgr->StartAnalysis("local", chain);
+    } else {
+        // if we want to run on grid, we create and configure the plugin
+        AliAnalysisAlien *alienHandler = new AliAnalysisAlien();
+        // also specify the include (header) paths on grid
+        alienHandler->AddIncludePath("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/include -I$ALICE_PHYSICS/include");
+        // make sure your source files get copied to grid
+        // alienHandler->SetAdditionalLibs("AliAnalysisTaskMyTask.cxx AliAnalysisTaskMyTask.h");
+        // alienHandler->SetAnalysisSource("AliAnalysisTaskMyTask.cxx");
+        // select the aliphysics version. all other packages
+        // are LOADED AUTOMATICALLY!
+        //alienHandler->SetAliPhysicsVersion("vAN-20181028_ROOT6-1");
+        alienHandler->SetAliPhysicsVersion("vAN-20191002_ROOT6-1");
+
+        // set the Alien API version
+        alienHandler->SetAPIVersion("V1.1x");
+        // select the input data
+/*
+        alienHandler->SetGridDataDir("/alice/data/2017/LHC17p");
+        alienHandler->SetDataPattern("*pass1_FAST/AOD208/0001/AliAOD.root");
+        // MC has no prefix, data has prefix 000
+        alienHandler->SetRunPrefix("000");
+        // runnumber
+        alienHandler->AddRunNumber(282341);
+*/
+
+        // path = /alice/sim/2016/LHC16h3/10/244480/AOD/088/AliAOD.root
+        alienHandler->SetGridDataDir("/alice/sim/2016/LHC16h3/17/");
+        alienHandler->SetDataPattern("*AOD/*AliAOD.root");
+        // MC has no prefix, data has prefix 000
+        alienHandler->SetRunPrefix("");
+        // runnumber
+        // based on pass3 !!!
+        alienHandler->AddRunNumber(244628);
+        alienHandler->AddRunNumber(244627);
+        alienHandler->AddRunNumber(244626);
+        alienHandler->AddRunNumber(244619);
+        alienHandler->AddRunNumber(244618);
+        alienHandler->AddRunNumber(244617);
+        alienHandler->AddRunNumber(244542);
+        alienHandler->AddRunNumber(244540);
+        alienHandler->AddRunNumber(244531);
+        alienHandler->AddRunNumber(244484);
+        alienHandler->AddRunNumber(244483);
+        alienHandler->AddRunNumber(244482);
+        alienHandler->AddRunNumber(244481);
+        alienHandler->AddRunNumber(244480);
+        alienHandler->AddRunNumber(244456);
+        alienHandler->AddRunNumber(244453);
+        alienHandler->AddRunNumber(244421);
+        alienHandler->AddRunNumber(244416);
+        alienHandler->AddRunNumber(244377);
+        alienHandler->AddRunNumber(244364);
+        alienHandler->AddRunNumber(244359);
+        alienHandler->AddRunNumber(244355);
+        alienHandler->AddRunNumber(244351);
+        //alienHandler->AddRunNumber(244343); // not in RunList_*_calo
+        alienHandler->AddRunNumber(244340);
+
+        // number of files per subjob
+        alienHandler->SetSplitMaxInputFileNumber(3);
+        alienHandler->SetExecutable("myTask.sh");
+        // specify how many seconds your job may take
+        alienHandler->SetTTL(54000); // max 86000
+        alienHandler->SetJDLName("myTask.jdl");
+
+        alienHandler->SetOutputToRunNo(kTRUE);
+        alienHandler->SetKeepLogs(kTRUE);
+
+
+        alienHandler->SetCheckCopy(kFALSE); // ### !!!
+        // merging: run with kTRUE to merge on grid
+        // after re-running the jobs in SetRunMode("terminate")
+        // (see below) mode, set SetMergeViaJDL(kFALSE)
+        // to collect final results
+        alienHandler->SetMaxMergeStages(3);
+        alienHandler->SetMergeViaJDL(kFALSE);  // ### !!!
+
+        // define the output folders
+        alienHandler->SetGridWorkingDir("myWorkingDir_LHC16h3/ptbin17");
+        alienHandler->SetGridOutputDir("myOutputDir");
+
+        // connect the alien plugin to the manager
+        mgr->SetGridHandler(alienHandler);
+        if(gridTest) {
+            // speficy on how many files you want to run
+            alienHandler->SetNtestFiles(1);
+            // and launch the analysis
+            alienHandler->SetRunMode("test");
+            mgr->StartAnalysis("grid");
+        } else {
+            // else launch the full grid analysis
+            alienHandler->SetRunMode("terminate");  // ### !!!
+            mgr->StartAnalysis("grid");
+        }
+    }
+}
