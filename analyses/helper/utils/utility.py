d8ca387668ea3b50d91fe4d820b9517fabb72374
sbysiak <sebastian.bysiak@gmail.com>
2020-03-14 11:30:13 +0100

    Add saving scaler to comet

--- analyses/helper/utils/utility.py
+++ analyses/helper/utils/utility.py
@@ -8,7 +8,7 @@ def printmd(string):
     
 
  
-def save_model(model, feat_names, exp=None, comet_name='model'):
+def save_model(model, feat_names, scaler, exp=None, comet_name='model'):
     """ pickles a model and list of feature names to "tmp/" dir and if (exp is passed) uploads it to comet.ml
     
     Parameters
@@ -17,6 +17,8 @@ def save_model(model, feat_names, exp=None, comet_name='model'):
         model object
     feat_names : iterable of strings
         array of features' names used in training
+    scaler : sklearn-compatible scaler e.g. StandardScaler
+        scaler used to scale training data
     exp : comet_ml.Experiment
         experiment to log model in
     comet_name : str
@@ -33,12 +35,15 @@ def save_model(model, feat_names, exp=None, comet_name='model'):
     
     fpath_model = f'tmp/model__{random_str()}.pickle'
     fpath_featnames = fpath_model.replace('.pickle', '__feat_names.pickle')
+    fpath_scaler    = fpath_model.replace('.pickle', '__scaler.pickle')
     pickle.dump(model, open(fpath_model, 'wb'))
     pickle.dump(feat_names, open(fpath_featnames, 'wb'))
+    pickle.dump(scaler, open(fpath_scaler, 'wb'))
     
     if exp:
         exp.log_model(name=comet_name, file_or_folder=fpath_model, file_name='model.pickle')
         exp.log_model(name=comet_name, file_or_folder=fpath_featnames, file_name='feat_names.pickle')
+        exp.log_model(name=comet_name, file_or_folder=fpath_scaler, file_name='scaler.pickle')
         
-    return fpath_model, fpath_featnames
+    return fpath_model, fpath_featnames, fpath_scaler
 
