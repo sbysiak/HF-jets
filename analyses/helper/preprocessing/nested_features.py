9338d0a41a55e78ff680cbf3821ae9f02551dfdd
sbysiak <sebastian.bysiak@gmail.com>
2020-04-10 11:22:30 +0200

    Add apply_cut() to preprocessing
    Functionality to filter index column with some cuts,
    e.g. sort SV by Lxy but only if Chi2 < 5

--- analyses/helper/preprocessing/nested_features.py
+++ analyses/helper/preprocessing/nested_features.py
@@ -6,26 +6,30 @@ All operations happen inplace!
 Adding features in steps:
 1. create features which you want to sort or to sort-by them, this includes transformations like `abs` or `log`
 2. create indexes `add_sorting_index()`
-3. create sorted columns `add_sorted_col()`
-4. fill sorted values to new columns: `add_nth_val()`
-5. As last step remeber to exclude temporary columns from data set!
+3. filter indices arrays `apply_cuts()`
+4. create sorted columns `add_sorted_col()`
+5. fill sorted values to new columns: `add_nth_val()`
+6. As last step remeber to exclude temporary columns from data set!
 
-E.g. in order to have $p_T$, $\eta$ and $\phi$ of first 5 tracks sorted by abs(IPd) do:
+E.g. in order to have $p_T$, $\eta$ and $\phi$ of first 5 tracks sorted by abs(IPd) with -0.9 < $\eta$ < 0.9 do:
 ```
 # Ad 1
 df['Jet_Track_IPdAbs'] = df.apply(lambda row: abs(row['Jet_Track_IPd']), axis=1)
 # Ad 2
 add_sorting_index(df, 'Jet_Track_IPdAbs', 'desc')
 # Ad 3
+apply_cut(df, 'Jet_Track_Eta > -0.9', 'IPdAbs', 'desc')
+apply_cut(df, 'Jet_Track_Eta < 0.9' , 'IPdAbs', 'desc')
+# Ad 4
 add_sorted_col(df, 'Jet_Track_Pt',  'IPdAbs', 'desc')
 add_sorted_col(df, 'Jet_Track_Phi', 'IPdAbs', 'desc')
 add_sorted_col(df, 'Jet_Track_Eta', 'IPdAbs', 'desc')
-# Ad 4
+# Ad 5
 for i in range(5):
     add_nth_val(df, col_name=f'Jet_Track_Pt__sortby__IPdAbs__desc' , n=i, fillna=None)
     add_nth_val(df, col_name=f'Jet_Track_Phi__sortby__IPdAbs__desc', n=i, fillna=None)
     add_nth_val(df, col_name=f'Jet_Track_Eta__sortby__IPdAbs__desc', n=i, fillna=None)
-# Ad 5
+# Ad 6
 df.drop(['Index__Jet_Track__sortby__IPdAbs__desc', 'Jet_Track_Pt__sortby__IPdAbs__desc', 'Jet_Track_Phi__sortby__IPdAbs__desc', 'Jet_Track_Eta__sortby__IPdAbs__desc'], inplace=True)
 ```
 """
@@ -116,3 +120,30 @@ def add_nth_val(df, col_name, n, fillna=None):
     kind = _colname2kind(col_name)
     new_col_name = col_name.replace(kind, f'{kind}_{n}')
     df[new_col_name] = [arr[n] if n<len(arr) else fillna for arr in df[col_name]]
+
+
+def apply_cut(df, cut, sortby, order):
+    """ filters index according to cut on other column
+    Parameters
+    ----------
+    df : pd.DataFrame
+        input data
+    cut : str
+        form: '[feature] [>/<] [value]' e.g. 'Jet_SecVtx_Chi2 < 10'
+    sortby : str
+        feature of obj (like track or SV) which will determine new order
+    order : str
+        'desc' or 'asc'
+    """
+    if len(cut.split(' ')) != 3 or cut.split(' ')[1] not in ['<', '>', '<=', '>=']:
+        raise ValueError("incorrect cut str format, it should be sth like: e.g. \'Jet_SecVtx_Chi2 < 10\' ")
+    cut_colname, gt_lt, cut_value = cut.split(' ')
+    cut_value = float(cut_value)
+    kind = _colname2kind(cut_colname)
+    index_name = _form_index_name(kind, sortby, order)
+    if gt_lt == '>':
+        df[index_name] =                 df.apply(lambda row: [idx for idx, val in zip(row[index_name], np.array(row[cut_colname])[row[index_name]]) if val > cut_value], axis=1)
+    elif gt_lt == '<':
+        df[index_name] = df.apply(lambda row: [idx for idx, val in zip(row[index_name], np.array(row[cut_colname])[row[index_name]]) if val < cut_value], axis=1)
+
+
